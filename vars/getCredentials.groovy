ae7ceb2f5aa2013885dc0a1499234e4b9077d693
Uwe Peuker <dev@upeuker.net>
2021-12-13 21:35:35 +0100

    neue Funktion

--- vars/getCredentials.groovy
+++ vars/getCredentials.groovy
@@ -39,23 +39,10 @@ def call(Map config = [:]) {
 			res.close()
 		}
 	}
-
-	def stringMap = "['a':2,'b':4]"
-	def map = evaluate(stringMap)
-	
-	assert map.a == 2
-	assert map.b == 4
-	
-	def stringMapNested = "['foo':'bar', baz:['alpha':'beta']]"
-	def map2 = evaluate(stringMapNested)
-	
-	assert map2.foo == "bar"
-	assert map2.baz.alpha == "beta"
 	
 	def String key = "${JENKINS_URL}"
-	def envDefs = props.getProperty(key, "['a':2,'b':4]");
+	def envDefs = props.getProperty(key, "[]");
 	def envMap = evaluate(envDefs)
-	envMap.each{ k, v -> println "${k}:${v}" }
 	envMap.each{k, v -> createGlobalEnvironmentVariables(k.toString(),v.toString())}
 	
 }
