a84520c5862fdff72b13d8aa754fd333f5dd2c9a
Uwe Peuker <dev@upeuker.net>
2021-12-13 20:57:36 +0100

    neue Funktion

---
+++ vars/getCredentials.groovy
@@ -0,0 +1,24 @@
+import groovy.yaml.YamlSlurper
+
+def call(Map config = [:]) {
+	def content = libraryResource 'config/jenkinsCredentialMappings.yaml'
+	def config = new YamlSlurper().parseText(configYaml)
+
+	sh "echo ${config}"	
+}
+
+def cfffall(Map config = [:]) {
+	
+	def content = libraryResource 'config/jenkinsCredentialMappings.properties'
+	Properties props = new java.util.Properties()
+	
+	new java.io.StringReader(content).with { res ->
+		try {
+			props.load(res)
+		} finally {
+			res.close()
+		}
+	}
+
+	return props.getProperty("$(JENKINS_URL)", config.get("default", "undefined"));
+}
