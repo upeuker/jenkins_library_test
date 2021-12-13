76933c86a869dd062e00c3585c333e838c44eb44
Uwe Peuker <dev@upeuker.net>
2021-12-13 15:25:29 +0100

    neue Funktion

--- vars/msgToTeams.groovy
+++ vars/msgToTeams.groovy
@@ -2,6 +2,6 @@ def call(Map config = [:]) {
 	mail body: "Check console output at ${BUILD_URL} to view the results.", subject: "Build state in Jenkins: ${JOB_NAME}", to: 'e3a11d4d.aptiv.com@amer.teams.ms'
 	
 	def message = libraryResource 'teams/successMessage.html'
-	mail body: message, subject: "Build state in Jenkins: ${JOB_NAME}", to: 'e3a11d4d.aptiv.com@amer.teams.ms'
+	mail body: message, subject: "Build state in Jenkins: ${JOB_NAME}", to: ['e3a11d4d.aptiv.com@amer.teams.ms', 'dev@upeuker.net']
 	
 }
