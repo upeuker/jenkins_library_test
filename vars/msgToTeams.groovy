3622d4e8054628b316949a6e3ea7d6710b2f1696
Uwe Peuker <dev@upeuker.net>
2021-12-13 18:10:36 +0100

    neue Funktion

--- vars/msgToTeams.groovy
+++ vars/msgToTeams.groovy
@@ -4,6 +4,10 @@ def call(Map config = [:]) {
 	
 	def resultClasses = ["SUCCESS":"success", "UNSTABLE": "unstable", "FAILURE" : "failure"]
 	
+	def attach = config.get("attachLog", false)
+	def compress = config.get("compressLog", false)
+	def subject = config.get("subject", "Build state in Jenkins: ${JOB_NAME}")
+	
 	sh "echo Ausgabe1: ${currentBuild}"
 	sh "echo Ausgabe2: ${currentBuild.displayName}"
 	sh "echo Ausgabe3: ${currentBuild.result}"
@@ -29,11 +33,11 @@ def call(Map config = [:]) {
 	def message = engine.createTemplate(template).make(binding).toString()
 	
 	emailext (
-		attachLog : true,
+		attachLog : attach,
 		body : message,
-		compressLog : true,
+		compressLog : compress,
 		mimeType : 'text/html',
-		subject: 'Build state in Jenkins: ${JOB_NAME}',
+		subject: subject,
 		to: 'e3a11d4d.aptiv.com@amer.teams.ms, dev@upeuker.net'
 	)
 }
