c34022f4815dcfa2465e2b2bee74d118090d8e8b
Uwe Peuker <dev@upeuker.net>
2021-12-13 17:02:20 +0100

    neue Funktion

--- vars/msgToTeams.groovy
+++ vars/msgToTeams.groovy
@@ -2,12 +2,12 @@ def call(Map config = [:]) {
 	
 	def message = libraryResource 'teams/message_template.html'
 	
-	emailext 
+	emailext (
 		attachLog : true,
 		body : message,
 		compressLog : true,
 		mimeType : 'text/html',
 		subject: 'Build state in Jenkins: ${JOB_NAME}',
 		to: 'e3a11d4d.aptiv.com@amer.teams.ms, dev@upeuker.net'
-	
+	)
 }
