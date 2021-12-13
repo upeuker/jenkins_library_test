cf9ad3a92d9b7fb3c201202144733702e2672bfa
Uwe Peuker <dev@upeuker.net>
2021-12-13 21:58:09 +0100

    neue Funktion

--- vars/getCredentials.groovy
+++ vars/getCredentials.groovy
@@ -11,7 +11,9 @@ def call(Map config = [:]) {
 		}
 	}
 	
-	def String request = "${JENKINS_URL}"
+	def java.net.URL url = "${JENKINS_URL}"
+	
+	def String request = url.getHost()
 	def String key = config.get("key", "");
 	if(!key.isEmpty()) {
 		request = request + "_" + key;
