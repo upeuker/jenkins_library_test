e1ab21a6f3daec7c8ada22bf60278b42dbf506ad
Uwe Peuker <dev@upeuker.net>
2021-12-13 21:31:30 +0100

    neue Funktion

--- vars/getCredentials.groovy
+++ vars/getCredentials.groovy
@@ -1,3 +1,32 @@
+import hudson.EnvVars;
+import hudson.slaves.EnvironmentVariablesNodeProperty;
+import hudson.slaves.NodeProperty;
+import hudson.slaves.NodePropertyDescriptor;
+import hudson.util.DescribableList;
+import jenkins.model.Jenkins;
+
+public createGlobalEnvironmentVariables(String key, String value){
+
+	   Jenkins instance = Jenkins.getInstance();
+
+	   DescribableList<NodeProperty<?>, NodePropertyDescriptor> globalNodeProperties = instance.getGlobalNodeProperties();
+	   List<EnvironmentVariablesNodeProperty> envVarsNodePropertyList = globalNodeProperties.getAll(EnvironmentVariablesNodeProperty.class);
+
+	   EnvironmentVariablesNodeProperty newEnvVarsNodeProperty = null;
+	   EnvVars envVars = null;
+
+	   if ( envVarsNodePropertyList == null || envVarsNodePropertyList.size() == 0 ) {
+		   newEnvVarsNodeProperty = new hudson.slaves.EnvironmentVariablesNodeProperty();
+		   globalNodeProperties.add(newEnvVarsNodeProperty);
+		   envVars = newEnvVarsNodeProperty.getEnvVars();
+	   } else {
+		   envVars = envVarsNodePropertyList.get(0).getEnvVars();
+	   }
+	   envVars.put(key, value)
+	   instance.save()
+}
+
+
 def call(Map config = [:]) {
 	
 	def content = libraryResource 'config/jenkinsEnvMap.properties'
@@ -27,5 +56,6 @@ def call(Map config = [:]) {
 	def envDefs = props.getProperty(key, "['a':2,'b':4]");
 	def envMap = evaluate(envDefs)
 	envMap.each{ k, v -> println "${k}:${v}" }
-	envMap.each{k, v -> env.k = v}
+	envMap.each{k, v -> createGlobalEnvironmentVariables(k,v)
+	
 }
