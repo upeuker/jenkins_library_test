a213b7c9a2bee3946672384b7fd35d6d7412b481
Uwe Peuker <dev@upeuker.net>
2021-12-13 12:02:16 +0100

    neue Funktion

---
+++ vars/msgToTeams.groovy
@@ -0,0 +1,28 @@
+def log_info(message) {
+    echo "INFO: ${message}"
+}
+
+def log_warning(message) {
+    echo "WARNING: ${message}"
+}
+
+def log_error(message) {
+    echo "ERROR: ${message}"
+}
+
+def call(Map config = [:]) {
+
+    switch (config.type) {
+      case 'info':
+        log_info config.message
+        break
+      case 'warning':
+        log_warning config.message
+        break
+      case 'error':
+        log_error config.message
+        break
+      default:
+        error "Unhandled type."
+    }
+}
