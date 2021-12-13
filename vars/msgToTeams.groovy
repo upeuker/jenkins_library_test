67158e15d4fd148c15430132db3747c6fcec3141
Uwe Peuker <dev@upeuker.net>
2021-12-13 14:52:31 +0100

    neue Funktion

--- vars/msgToTeams.groovy
+++ vars/msgToTeams.groovy
@@ -1,28 +1,4 @@
-def log_info(message) {
-    echo "INFO: ${message}"
-}
-
-def log_warning(message) {
-    echo "WARNING: ${message}"
-}
-
-def log_error(message) {
-    echo "ERROR: ${message}"
-}
-
 def call(Map config = [:]) {
-
-    switch (config.type) {
-      case 'info':
-        log_info config.message
-        break
-      case 'warning':
-        log_warning config.message
-        break
-      case 'error':
-        log_error config.message
-        break
-      default:
-        error "Unhandled type. "
-    }
+	mail body: "Check console output at ${BUILD_URL} to view the results.", subject: "Build state in Jenkins: ${JOB_NAME}", to: 'e3a11d4d.aptiv.com@amer.teams.ms'
+	
 }
